19d0911e023d071cf2f40178d2b3bfa2a40496e0
博大大 <1259821073@qq.com>
2018-12-07 08:46:02 +0800

    客户和交易
    任亚博

--- js/transaction.js
+++ js/transaction.js
@@ -1,38 +1,70 @@
-var rybNavOffOn = document.getElementById("rybNavOne");
-var rybBoxShadowMask = document.getElementById("rybBoxShadowMask");
-var rybTopNavDaoru = document.getElementById("rybTopNavDaoru");
-var rybBoxShadowContain = document.getElementById("rybBoxShadowContain");
-var rybclick = document.getElementById("rybclick");
-var rybbian = document.getElementById("rybbian");
 var off = true;
 document.onclick = function (evt) {
     var event = evt || window.event;
-    console.log(event);
     var a = event.target || event.srcElement;
     if (a.id == "rybclick") {
-        rybbian.style.cssText = "display:block;";
-        rybBoxShadowMask.style.cssText = "display:none;";
-        rybBoxShadowContain.style.cssText = "display:none;";
+        $("#rybbian").css("display", "block");
+        $("#rybBoxShadowMask").css("display", "none");
+        $("#rybBoxShadowContain").css("display", "none");
     } else if (a.id == "rybNavOne") {
         if (off) {
             off = false;
-            rybBoxShadowContain.style.cssText = "display:block;";
-            rybbian.style.cssText = "display:none;";
-            rybBoxShadowMask.style.cssText = "display:none;";
+            $("#rybBoxShadowContain").css("display", "block");
+            $("#rybbian").css("display", "none");
+            $("#rybBoxShadowMask").css("display", "none");
         } else {
-            rybbian.style.cssText = "display:none;";
-            rybBoxShadowMask.style.cssText = "display:none;";
-            rybBoxShadowContain.style.cssText = "display:none;";
+            $("#rybbian").css("display", "none");
+            $("#rybBoxShadowMask").css("display", "none");
+            $("#rybBoxShadowContain").css("display", "none");
             off = true;
         }
     } else if (a.id == "rybTopNavDaoru") {
-        rybBoxShadowMask.style.cssText = "display:block;";
-        rybbian.style.cssText = "display:none;";
-        rybBoxShadowContain.style.cssText = "display:none;";
+        $("#rybBoxShadowMask").css("display", "block");
+        $("#rybbian").css("display", "none");
+        $("#rybBoxShadowContain").css("display", "none");
     } else {
         off = true;
-        rybbian.style.cssText = "display:none;";
-        rybBoxShadowMask.style.cssText = "display:none;";
-        rybBoxShadowContain.style.cssText = "display:none;";
+        $("#rybbian").css("display", "none");
+        $("#rybBoxShadowMask").css("display", "none");
+        $("#rybBoxShadowContain").css("display", "none");
     }
+};
+$("#rybContentForm ul li input").on("click", function () {
+    var nextHeight = parseInt($(this).next().next().height());
+    var a = $(this).parent("li").height();
+    if ($(this).prop("checked")) {
+        a = a + nextHeight;
+        $(this).parent("li").css("height", a + "px");
+    } else {
+        $(this).parent("li").css("height", "29px");
+    }
+    for (var i = 0; i < $("#rybContentForm ul li input").length; i++) {
+        if ($(this).prop("checked") == true) {
+            $(".ryb_lv_filter_options").css("display", "block");
+            break;
+        } else {
+            $(".ryb_lv_filter_options").css("display", "none");
+            break;
+        }
+    }
+});
+$(document).ready(function () {
+    $(".newwhitebtn").click(function () {
+        $(".ryb_boxShadowMask").slideDown(500);
+    });
+    $("#rybclick").click(function () {
+        $("#rybbian").slideDown(500);
+    });
+});
+var c = document.documentElement.clientHeight;
+window.onload = function () {
+    $("#rybContentForm").css("height", c * 0.9 + "px");
+    $("#rybContentForm ul").css("height", "80%");
+}
+window.onresize = function () {
+    c = document.documentElement.clientHeight;
+    console.log(1);
+    $("#rybContentForm").css("height", c * 0.9 + "px");
+    console.log($("#rybContentForm").height());
+    $("#rybContentForm ul").css("height", "80%");
 }
