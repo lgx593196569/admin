686b379f5a1afd9cf38cd1dafd63eb4111a72f2f
lgx593196569 <593196569@qq.com>
2018-12-10 14:23:46 +0800

    修复bug

--- js/form.js
+++ js/form.js
@@ -2,12 +2,7 @@
 //     $(".bb_one input").css("check")
 // })
 
-$(".list ul li a").on("click",function(){
-    $(this).css(
-        "color","#d4dbe1"
-    )
-})
-$(".list ul li").on("click",function(){
-    $(this).css("background-color","#68abe3");
-    $(this).children(css("color","#d4dbe1"));
-})
+// $(".list ul li").on("click",function(){
+//     $(this).css("background-color","#68abe3");
+//     $(this).children(css("color","#d4dbe1"));
+// })
